cbf51f1ec1f46f2aff37817407a6d8b8e35f6d55
장윤호 <yunho.jang@wizcore.co.kr>
2021-11-22 17:55:16 +0900

    created global style

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Global, css } from "@emotion/react";
+
 import App from 'App';
+
+const globalStyle = css`
+  * {
+    box-sizing: border-box;
+  }
+
+  html,body  {
+    margin: 0;
+    padding: 0;
+  }
+
+  button {
+    cursor: pointer;
+    border: none;
+    background: none;
+  }
+`;
 
 ReactDOM.render(
   <React.StrictMode>
+    <Global styles={globalStyle}/>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
