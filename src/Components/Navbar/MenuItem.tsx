d7540682fc3fc6bd368064976af737057a6119cd
장윤호 <yunho.jang@wizcore.co.kr>
2021-11-24 11:29:13 +0900

    아이콘 클릭 시 메뉴 열림

--- src/Components/Navbar/MenuItem.tsx
+++ src/Components/Navbar/MenuItem.tsx
@@ -2,7 +2,7 @@ import { FC, useContext } from "react";
 import { css } from "@emotion/css";
 
 import { color_dark } from "style/theme";
-import { ThemeContext } from "App";
+import { MenuContext, ThemeContext } from "App";
 
 interface MenuItemProps {
   menu:any
@@ -10,15 +10,24 @@ interface MenuItemProps {
 }
  
 const MenuItem: FC<MenuItemProps> = ({menu, isOpen}) => {
+  const {open, toggle} = useContext(MenuContext);
   const {isDark} = useContext(ThemeContext);
 
   const dark = isDark ? 'dark' : '';
   const iconCls = `${icon} ${dark}`;
   const linkCls = `${link} ${dark}`;
 
+  const onClick = () => {
+    if (open) {
+      return null;
+    }
+
+    toggle();
+  }
+
   return (
     <div className={item}>
-      <div className={iconCls}></div>
+      <div className={iconCls} onClick={onClick}></div>
 
       {isOpen && <div className={linkCls}>{menu.name}</div>}
     </div>
@@ -31,6 +40,7 @@ const item = css`
   display: flex;
   align-items: center;
   gap: 1em;
+  cursor: pointer;
 `;
 
 const icon = css`
@@ -47,7 +57,6 @@ const link = css`
   width: 8em;
   transform-origin: left;
   animation: .2s slide cubic-bezier(0,0,0,1);
-  cursor: pointer;
   color: #5e6e82;
 
   &.dark {
